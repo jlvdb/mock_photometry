aa34e2bdb244e87a29cab6e4898ef1f6741b10ec
Jan Luca van den Busch <jl.vdb@astro.rub.de>
2023-04-16 21:36:06 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
-import os
 import setuptools
 
 
-with open("README.md", "r") as f:
-    long_description = f.read()
 with open("requirements.txt") as f:
     install_requires = [pkg.strip() for pkg in f.readlines() if pkg.strip()]
-scripts = [
-    os.path.join("bin", fname) for fname in os.listdir("bin")]
 
 setuptools.setup(
-    name="MICE2_mocks",
-    version="1.1",
+    name="mock_photometry",
+    version="1.0",
     author="Jan Luca van den Busch",
-    description="This Repository provides code to create galaxy mock catalogues based on MICE galaxy catalogues.",
-    long_description=long_description,
+    description="Analytic prescription to simulate photometric noise in simulated galaxy catalogs.",
     long_description_content_type="text/markdown",
-    url="https://github.com/KiDS-WL/MICE2_mocks",
+    url="https://github.com/jlvdb/mock_photometry",
     packages=setuptools.find_packages(),
-    include_package_data=True,
-    scripts=scripts,
     install_requires=install_requires)
